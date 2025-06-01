0da86fec509d501f04462baba2fda8615cd531f6
6r4m <210684602+6r4m@users.noreply.github.com>
2025-06-01 06:16:44 -0400

    experimenting with deploying dashboards

--- data_pipeline.py
+++ data_pipeline.py
@@ -2,8 +2,9 @@ import api_calls as api_calls
 import pandas as pd
 
 dfl = []
-year_list = [2020, 2021, 2022, 2023, 2024]
-for y in year_list:
+years = list(range(2010, 2025, 1))
+
+for y in years:
     start_date = f"{y-1}-10-01"
     end_date = f"{y}-09-30"
     json = api_calls.get_spending_by_geography(start_date, end_date)
